e59f0dc52ec7fd7fa060dce8f75a79e9ab1cc0ce
Sarvesh2005-code <sarveshnakhale21@gmail.com>
2026-02-17 21:37:05 +0530

    Initial commit : Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.sarvesh.javabasics;
+
+public class HelloWorld {
+/*
+ * Some point which must consider before writing java code
+ * class name must be in upper case and also the filename must be same
+ * methods are in camel case
+ * and constants are in whole capital separated with underscore.*/
+	
+	public static void main(String[] args) {
+		
+		// Hello World Print Statement
+		
+		System.out.println("Hello World !!!");
+
+	}
+
+}
