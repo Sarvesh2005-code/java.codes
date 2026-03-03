b2996b2f591901797171146a21e7f7738500b1bf
Sarvesh2005-code <sarveshnakhale21@gmail.com>
2026-03-03 21:34:06 +0530

    Move all zeros to end

---
+++ MoveZeros.java
@@ -0,0 +1,41 @@
+package com.sarvesh.javabasics;
+
+import java.util.Scanner;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        moveZeros(arr);
+
+        System.out.println("Array after moving zeros:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+
+    public static void moveZeros(int[] arr) {
+
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+    }
+}
