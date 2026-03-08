6ace82aae4800cc2136b0b42de886a1fa7a82739
Sarvesh2005-code <sarveshnakhale21@gmail.com>
2026-03-08 22:10:27 +0530

    Palindrome in java using two pointers

---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+package com.sarvesh.javabasics;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        sc.close();
+    }
+}
