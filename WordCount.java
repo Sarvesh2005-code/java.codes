ca73dcd8039ff1c2dbb3ec6b38e03166bbb7e5b3
Sarvesh2005-code <sarveshnakhale21@gmail.com>
2026-03-18 22:34:50 +0530

    Word counter in java

---
+++ WordCount.java
@@ -0,0 +1,20 @@
+package com.sarvesh.javabasics;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+        for(int i=0;i<str.length();i++) {
+            if(str.charAt(i) != ' '&& (i==0||str.charAt(i-1)==' ')) {
+                count++;
+            }
+        }
+        System.out.println("Words = " + count);
+       sc.close();
+    }
+}
